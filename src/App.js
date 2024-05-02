2ad4f2d05e238c9882477d1f2d1921058d733d9f
Mirza Sabaria Nisha <138323973+M-Sabaria@users.noreply.github.com>
2024-05-03 04:18:16 +0600

    only 2 links r  not working

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Navbar from "./Components/Navbar";
 function App() {
   return (
     <div className="App">
-      <Router basename="/home">
+      <Router basename="/">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
